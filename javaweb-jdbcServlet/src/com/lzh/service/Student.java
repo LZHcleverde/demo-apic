a5bd960b9d120289c947a4ddb6b2eaf90ae49762
lzh <1762259843@qq.com>
2023-07-01 16:50:15 +0800

    fix - commit 1

--- javaweb-jdbcServlet/src/com/lzh/service/Student.java
+++ javaweb-jdbcServlet/src/com/lzh/service/Student.java
@@ -2,8 +2,6 @@ package com.lzh.service;
 
 public class Student {
     public static void main(String[] args) {
-        System.out.println("hello Git3");
-        System.out.println("hello Git3");
-
+        System.out.println("javaweb - fix");
     }
 }
