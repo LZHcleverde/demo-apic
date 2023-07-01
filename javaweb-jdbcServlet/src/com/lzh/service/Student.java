d5cd0daa9dd2a39325cc70c015c91e4d89e3479c
lzh <1762259843@qq.com>
2023-07-01 16:37:52 +0800

    second commit

--- javaweb-jdbcServlet/src/com/lzh/service/Student.java
+++ javaweb-jdbcServlet/src/com/lzh/service/Student.java
@@ -5,5 +5,7 @@ public class Student {
         System.out.println("hello Git");
         System.out.println("hello Git");
         System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
     }
 }
