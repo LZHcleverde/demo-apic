0608b4f47fb0aaf37983c7bbc1b5b1e2bec7c7db
lzh <1762259843@qq.com>
2023-07-01 17:21:20 +0800

    002

---
+++ zspringboot-002-starter/src/main/java/com/lzh/boot/Zspringboot002StarterApplication.java
@@ -0,0 +1,13 @@
+package com.lzh.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zspringboot002StarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zspringboot002StarterApplication.class, args);
+    }
+
+}
---
+++ zspringboot-002-starter/src/test/java/com/lzh/boot/Zspringboot002StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzh.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zspringboot002StarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
